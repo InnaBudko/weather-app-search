05b68ea7d0670b16bc249532f6076ae9b469b5f5
Inna Budko <inna.budko@gmail.com>
2023-01-28 19:44:11 +0200

    Updated day and time using data from response

--- src/app.js
+++ src/app.js
@@ -1,15 +1,26 @@
+function formatDate(timestamp) {
+  // calculate the date and time
+  let date = new Date(timestamp);
+  let day = date.toLocaleDateString("en-US", { weekday: "long" });
+  let hours = String(date.getHours()).padStart(2, "0");
+  let minutes = String(date.getMinutes()).padStart(2, "0");
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let datetimeElement = document.querySelector("#date-time");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  datetimeElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "a15d11bdb9d6ebc7982d6ab8606b3705";
