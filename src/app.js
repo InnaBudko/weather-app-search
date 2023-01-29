85eac85f7c86c95f70d50f5981359f4339d1e104
Inna Budko <inna.budko@gmail.com>
2023-01-29 17:08:37 +0200

    Update weather icon from response

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let datetimeElement = document.querySelector("#date-time");
+  let iconElement = document.querySelector("#weather-icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -21,9 +22,14 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   datetimeElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@4x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "a15d11bdb9d6ebc7982d6ab8606b3705";
+let apiKey = "aca4dd3643b89e94dbd3cac6cf6f2638";
 let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
